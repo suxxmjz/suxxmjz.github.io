6dadd992d5a672b25b82a3e73a14449395db6406
ss1106 <sukriti.sharma@ucalgary.ca>
2025-02-24 15:00:43 -0700

    adapt for light mode

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,4 +1,3 @@
-// app/layout.tsx
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
@@ -21,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-black text-white`}>
         <Navbar />
         {children}
       </body>
